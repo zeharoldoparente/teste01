988f9e6dac795a1602269c710b7b4d1384f75cad
José Aroldo <joseharoldoparente@gmail.com>
2024-08-14 17:12:50 -0400

    atualização importante

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Gestão de Produtos',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
